b028efd07b040ec74f7b1c5fbbcff269807b34f8
ahmedanan418 <ahmed.anan418@gmail.com>
2024-09-19 19:07:10 +0300

    mafish far2 la3abt shwya bs

--- install/turtlebot3_gazebo/share/turtlebot3_gazebo/launch/turtlebot3_my_world.launch.py
+++ install/turtlebot3_gazebo/share/turtlebot3_gazebo/launch/turtlebot3_my_world.launch.py
@@ -36,7 +36,7 @@ def generate_launch_description():
     world = os.path.join(
         get_package_share_directory('turtlebot3_gazebo'),
         'worlds',
-        'test_world.world'
+        'new_test_word.world'
     )
 
     gzserver_cmd = IncludeLaunchDescription(
